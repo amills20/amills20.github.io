161dd14843f34c3beca49bc972ce9642eccc75b7
alexc <amills20@georgefox.edu>
2022-07-24 11:09:06 -0700

    figured out how to use js to get something from the html

--- rps/app.js
+++ rps/app.js
@@ -1,3 +1,8 @@
 let test = document.getElementById("choice").onclick;
 
 console.log(test);
+
+function testFunction() {
+    let test2 = document.getElementById("choice").value;
+    console.log(test2);
+}
